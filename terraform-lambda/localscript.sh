18f45c390bc2a0c66b3d5959995be8de718a301d
dorukkakici <98922277+dorukkakici@users.noreply.github.com>
2025-02-02 19:47:38 -0600

    localscript.sh changed

--- terraform-lambda/localscript.sh
+++ terraform-lambda/localscript.sh
@@ -2,8 +2,8 @@
 
 sudo yum install wget -y
 wget https://releases.hashicorp.com/terraform/1.10.5/terraform_1.10.5_linux_amd64.zip
-unzip terraform_0.15.5_linux_amd64.zip
+unzip terraform_1.10.5_linux_amd64.zip
 chmod +x terraform
 sudo mv terraform /bin
 terraform version
-rm -rf terraform_0.15.5_linux_amd64.zip
+rm -rf terraform_1.10.5_linux_amd64.zip
