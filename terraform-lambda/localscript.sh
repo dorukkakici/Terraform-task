5fe317c005aab4b16b847dbdbb9dc78c34647ded
dorukkakici <98922277+dorukkakici@users.noreply.github.com>
2025-02-02 19:10:20 -0600

    new terraform version added

--- terraform-lambda/localscript.sh
+++ terraform-lambda/localscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo yum install wget -y
-wget https://releases.hashicorp.com/terraform/0.15.5/terraform_0.15.5_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/1.10.5/terraform_1.10.5_linux_amd64.zip
 unzip terraform_0.15.5_linux_amd64.zip
 chmod +x terraform
 sudo mv terraform /bin
